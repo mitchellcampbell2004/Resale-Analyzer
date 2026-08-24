13027b7fe8b2a963e91819dffb5dbd1388e79951
John Mitchell Campbell Jr <mitchellcampbell2004@gmail.com>
2026-08-23 22:00:05 -0700

    Initial project setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Resale Analyzer
+
+A project that will eventually become a full-blown web application whose purpose is to calculate the profitability of purchasing items for resale.
+
+## Current Features
+
+-None
+
+## Planned/Potential Features
+
+-Resale calculations
+-Web interface
+-User log ins
+-Inventory analysis
+-Break even analysis
+-Statistical analysis with R
+-Data visualization with R
+-Database
+-Machine learning
---
+++ calculations.py
@@ -0,0 +1,29 @@
+resale.py
+#Mitchell Campbell
+'''Purpose: to create an application utilizing Python and R in which one can enter information, calculate results, and display information related to reselling.'''
+
+#Home Screen; possible user/pw entering
+print("Welcome!")
+
+#explain the purpose
+print("The purpose of this program is to aid resellers with finding (potential) goods, calculating profitability, and to display the calculated information in an easily-digestible format.")
+
+#Home menu
+
+print("What would you like to do?")
+
+while True:
+    home_menu_choice = input('-See prior purchases/profits (enter "prior" or "history") \n-Calculate potential/new profits (enter "potential" or "new") \n-Other (enter "other") ').lower().strip()
+    if home_menu_choice in ('prior', 'history'):
+        print("Routing to history...")
+    elif home_menu_choice in ('potential', 'new'):
+        print("Routing to new order...")
+    elif home_menu_choice in ('other'):
+        print('Routing to "Other" menu...')
+    else:
+        print("Sorry, that is not a valid option.")
+        print("Please select one of the options listed on the menu.")
+
+if home_menu_choice in ('prior', 'history'):
+    print("Welcome to the potential/current ")
+#Enter information related to potential purchase
