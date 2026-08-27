149f2d3c4e3c82471d4635f25b7742e1cb747323
John Mitchell Campbell Jr <mitchellcampbell2004@gmail.com>
2026-08-26 23:07:55 -0700

    Updated research.py. Added ability to find book (final product) link, find book title, and working on finding number of products.

--- research.py
+++ research.py
@@ -44,25 +44,44 @@ for category in book_categories:
         category_soup = BeautifulSoup(category_page, 'html.parser')
         
         book_anchors = category_soup.find_all("h3") #isolate the specific products by their identifying variable; in this case, h3; can't do find a's first and then find h3s, because find as turns it into a list
-        #doesn't matter:   for book_anchor in book_anchors:
-            #get book page link; most of the stuff below this doesn't matter b/c the book links don't go by type, they go just in the overall catalogue
-            #doesn't matter   book_anchor_string = str(book_anchor)
-            #doesn't matter:   book_link = re.search("/../..(.+)/index", book_anchor_string) #[^/]+ = any character except /
-            #doesn't matter:  book_link = (book_link.group(1)) #need group because it'll return a match object string; group() gives you the entire regex match, group(1) gives you just the part you asked for
-            #doesn't matter:   category_link_portion = new_category_link[:-11] #subtract the last 11 char of new_cat_link so that I can get rid of index, creating first part of final book string
-            #doesn't matter:   book_link_portion = book_link + "/index.html" #add index to book link to create last part of final book link
-            # doesn't matter:   final_book_link = category_link_portion + book_link_portion #add links together to make final book string
-            #doesn't matter:   print(final_book_link)
-
-        #doesn't matter:  open book page (final product page) and parse data
-        #doesn't matter   book_link_page = urllib.request.urlopen(final_book_link, context=ctx).read()
-        #doesn't matter:   book_soup = BeautifulSoup(book_link_page, 'html.parser')
-        
-        
         book_soup_count = 0
-        if book_soup_count < 1:
-            print(book_soup)
-            book_soup_count += 1
+        book_availability_tag_count = 0
+        for book_anchor in book_anchors:
+        #get book page link; most of the stuff below this doesn't matter b/c the book links don't go by type, they go just in the overall catalogue
+            book_anchor_string = str(book_anchor)
+            book_link = re.search("/../..(.+)/index", book_anchor_string) #[^/]+ = any character except /
+            book_link = (book_link.group(1)) #need group because it'll return a match object string; group() gives you the entire regex match, group(1) gives you just the part you asked for
+            #category_link_sub_num = len(book_link + )
+            category_link_portion = "https://books.toscrape.com/catalogue"       #new_category_link[:-11] #reality: just input the catalogue portion, gonna add book next; my wish: subtract the last 11 char of new_cat_link so that I can get rid of index, creating first part of final book string (if it was how I'd structure it)
+            book_link_portion = book_link + "/index.html" #add index to book link to create last part of final book link
+            final_book_link = category_link_portion + book_link_portion #add links together to make final book string
+            #print(final_book_link)
+
+            #open book page (final product page) and parse data
+            book_link_page = urllib.request.urlopen(final_book_link, context=ctx).read()
+            book_soup = BeautifulSoup(book_link_page, 'html.parser')
+
+            #find book title
+            book_title_tag = book_soup("title")
+            book_title_tag_str = str(book_title_tag)
+            book_title = re.search(r">\s*(.*)\s*\|", book_title_tag_str) #need to add "\" to | b/c the | is a spec char in regex
+            book_title = (book_title.group(1))
+            book_title = book_title.strip() # or just book_title[:-1]  #there's a white space that hangs onto the end of the title, so need to delete that; found it by counting char in title and then printing char --> 1 char difference, which is why I subtracted 1 char
+
+
+            #find # of items in stock
+            book_availability_tags = book_soup("p", class_="instock availability") #to find class within tags, put tag first and then 'class_="{name of class}"'
+            for tag in book_availability_tags:
+                book_availability_str = str(tag)
+                book_availability_number = re.search(r"\d+", book_availability_str) #instead of trying to use r".*([0-9])" to find a number use r"\d+" --> means to find one or more digits (\d = digit)
+                final_book_avail_num = (book_availability_number.group()) #don't use group(1), use group() b/c there's no parentheses in the regex equation
+                if book_availability_tag_count < 1:
+                    print(final_book_avail_num) ###******Need to finish --> something about needing to restructure code to not use for loop b/c that's throwing errors
+                    book_availability_tag_count += 1
+
+            if book_soup_count < 1:
+                #print(book_availability_tag)
+                book_soup_count += 1
 
             
             
