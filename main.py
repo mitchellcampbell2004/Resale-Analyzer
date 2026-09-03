3bedea430c8fd902b4f5b7d99197b3c1cac60812
John Mitchell Campbell Jr <mitchellcampbell2004@gmail.com>
2026-09-03 04:46:49 -0700

    Added most category/subcategory selections

--- main.py
+++ main.py
@@ -10,19 +10,18 @@
 from calculations import sales_tax_dict
 
 #trying to make func to confirm item category so don't have to repeat 13 times
-def item_category_confirmation(item_category_choice, item_category_title):
+def item_category_confirmation(item_category_title):
     while True: 
-        electronic_confirmation = input('You have chosen "electronics", is this correct? ').strip().lower()
-        if electronic_confirmation in ("yes", "y"):
+        confirmation_answer = input(f'You have chosen "{item_category_title}", is this correct? ').strip().lower()
+        if confirmation_answer in ("yes", "y"):
             print("Great!")
-            break
-        elif electronic_confirmation in ("no", "n"):
+            return "y"
+        elif confirmation_answer in ("no", "n"):
             print("Ok. Please choose the correct category for the item.")
-            break
+            return "n"
         else:
             print("Sorry, that is not a valid input.")
             print('Please enter either "yes" or "no".')
-
 
 
 #######Main Code
@@ -79,7 +78,7 @@ if menu_choice == "calculations":
     #have user input whether or not they are looking to buy multiple types of items
     print('Now, are you looking to purchase one type of item or multiple types of items?' )
     while True:
-        multiple_types_of_items_choice = input('For one type, enter "one", "1", or "single". For multiple types, enter "multiple".' ).strip().lower()
+        multiple_types_of_items_choice = input('For one type, enter "one", "1", or "single". For multiple types, enter "multiple". ').strip().lower()
         if multiple_types_of_items_choice in ("1", "one", "single"):
             print("Great! That's much easier than multiple.")
             item_type_choice = "1"
@@ -94,40 +93,247 @@ if menu_choice == "calculations":
 
     #single item type calculation
     if item_type_choice == "1":
-        print("So, what kind of item is this? Which category does it best-fit below?")
+        print("So, what kind of item is this?")
         #choose item_category
         while True:
             item_category = input("-Electronics(1) -Automotive(2) -Clothing/Apparel(3) -Home/Furniture(4) -Collectibles(5) -Toys/Games(6) -Books/Media(7) -Sports/Outdoors(8) -Jewelry/Accessories(9) -Beauty/Personal Care(10) -Hobbies/Crafts(11) -Industrial/Business Equipment(12) -Miscellaneous(13) ").strip().lower()
             if item_category in ("electronics", "electronic", "1"):
-                item_category_name = "electronics"
-                while True: 
-                    electronic_confirmation = input('You have chosen "electronics", is this correct? ').strip().lower()
-                    if electronic_confirmation in ("yes", "y"):
-                        print("Great!")
-                        break
-                    elif electronic_confirmation in ("no", "n"):
-                        print("Ok. Please choose the correct category for the item.")
-                        break
-                    else:
-                        print("Sorry, that is not a valid input.")
-                        print('Please enter either "yes" or "no".')
-            if electronic_confirmation in no:
+                item_category_name = "Electronics"
+            elif item_category in ("auto", "automotive", "car", "2"):
+                item_category_name = "Automotive"
+            elif item_category in ("clothing", "apparel", "clothes", "shoes", "3"):
+                item_category_name = "Clothing/Accessories"
+            elif item_category in ("home", "furniture", "house", "home/furniture", "4"):
+                item_category_name = "Home/Furniture"
+            elif item_category in ("collectibles", "5"):
+                item_category_name = "Collectibles"
+            elif item_category in ("toys", "games", "toys/games", "6"):
+                item_category_name = "Toys/Games"
+            elif item_category in ("books", "media", "books/media", "7"):
+                item_category_name = "Books/Media"
+            elif item_category in ("sports", "outdoors", "sports/outdoors", "8"):
+                item_category_name = "Sports/Outdoors"
+            elif item_category in ("jewelry", "accessories", "jewelry/accessories", "9"):
+                item_category_name = "Jewelry/Accessories"
+            elif item_category in ("beauty", "personal", "personal care", "beauty/personal care", "10"):
+                item_category_name = "Beauty/Personal Care"
+            elif item_category in ("hobbies", "hobby", "crafts", "hobbies/crafts", "11"):
+                item_category_name = "Hobbies/Crafts"
+            elif item_category in ("industrial", "business", "equipment", "industrial equipment", "business equipment", "industrial/business equipment", "12"):
+                item_category_name = "Industrial/Business Equipment"
+            elif item_category in ("misc", "misc.", "miscellaneous", "13"):
+                item_category_name = "Miscellaneous"
+            else:
+                print("Sorry, that's not a valid option.")
+                print("Please choose an option from the menu below.")
+                print("Type either the name of the menu or its corresponding number.")
                 continue
-            elif electronic_confirmation in yes:
-                break       
+
+            #either move on to next step or restart if user chose wrong category
+            item_cat_conf_answer = item_category_confirmation(item_category_name)
+            if item_cat_conf_answer == "y":
+                item_classification = item_category
+                break
+            elif item_cat_conf_answer == "n":
+                continue
+             
+        #for the category of item, create subcategories and have user choose which one to access
+        #choose electronics subcategory
+        if item_classification == "Electronics":
+            while True:
+                print("What electronics subcategory does this item fit under?")
+                elect_sub_cat = input("-Smartphones(1) -Tablets(2) -Laptops(3) -Desktop Computers(4) -Computer Components(5) -Monitors(6) -TVs(7) -Cameras/Lenses(8) -Audio Equipment(9) -Gaming Consoles(10) -Gaming Accessories(11) -Smart Home Devices(12) -Wearables/Smartwatches(13) -Networking Equipment(14) -Cables/Adaptors(15) -Other(16) ").strip().lower()
+                if elect_sub_cat in ("smartphones", "phones", "1"):
+                    elect_sub_cat_name = "Smartphones"
+                elif elect_sub_cat in ("tablets", "2"):
+                    elect_sub_cat_name = "Tablets"
+                elif elect_sub_cat in ("laptop", "laptops", "3"):
+                    elect_sub_cat_name = "Laptops"
+                elif elect_sub_cat in ("desktop", "computers", "desktop computers", "4"):
+                    elect_sub_cat_name = "Desktop Computers"
+                elif elect_sub_cat in ("computer components", "components", "5"):
+                    elect_sub_cat_name = "Computer Components"
+                elif elect_sub_cat in ("monitors", "monitor", "6"):
+                    elect_sub_cat_name = "Monitors"
+                elif elect_sub_cat in ("tv", "tvs", "7"):
+                    elect_sub_cat_name = "TVs"
+                elif elect_sub_cat in ("cameras", "lenses", "cameras/lenses", "8"):
+                    elect_sub_cat_name = "Cameras/Lenses"
+                elif elect_sub_cat in ("audio", "audio equipment", "9"):
+                    elect_sub_cat_name = "Audio Equipment"
+                elif elect_sub_cat in ("consoles", "gaming consoles", "10"):
+                    elect_sub_cat_name = "Gaming Consoles"
+                elif elect_sub_cat in ("accessories", "gaming accessories", "11"):
+                    elect_sub_cat_name = "Gaming Accessories"
+                elif elect_sub_cat in ("smart home devices", "smart devices", "home", "home devices", "12"):
+                    elect_sub_cat_name = "Smart Home Devices"
+                elif elect_sub_cat in ("wearables", "smartwatches", "wearables/smartwatches", "13"):
+                    elect_sub_cat_name = "Wearables/Smartwatches"
+                elif elect_sub_cat in ("networking", "networking equipment", "14"):
+                    elect_sub_cat_name = "Networking Equipment"
+                elif elect_sub_cat in ("cables", "adaptors", "cables/adaptors", "15"):
+                    elect_sub_cat_name = "Cables/Adaptors"
+                elif elect_sub_cat in ("other", "16"):
+                    elect_sub_cat_name = "Other"
+                else:
+                    print("Sorry, that's not a valid option.")
+                    print("Please enter a listed subcategory or its corresponding number.")
+                    continue
+
+                #confirm electronic subcategory selection or restart
+                elect_sub_cat_name_answer = item_category_confirmation(elect_sub_cat_name)
+                if elect_sub_cat_name_answer == "y":
+                    elect_classification = elect_sub_cat_name
+                    break
+                elif elect_sub_cat_name_answer == "n":
+                    continue
+
+        #choose automotive subcategory
+        elif item_classification == "Automotive":
+            while True:
+                print("What automotive subcategory does this item fit under?")
+                auto_sub_cat = input("-Vehicles(1) -Car/Truck Parts(2) -Wheels/Tires(3) -Tools/Equipment(4) -Electronics/Audio(5) -Interior Accessories(6) -Exterior Accessories(7) -Performance Parts(8) -Maintenance Parts(9) -Accessories(10) -Other(11) ").strip().lower()
+                if auto_sub_cat in ("vehicles", "1"):
+                    auto_sub_cat_name = "Vehicles"
+                elif auto_sub_cat in ("car parts", "truck parts", "car/truck parts", "2"):
+                    auto_sub_cat_name = "Car/Truck Parts"
+                elif auto_sub_cat in ("wheels", "tires", "wheels/tires", "3"):
+                    auto_sub_cat_name = "Wheels/Tires"
+                elif auto_sub_cat in ("tools", "equipment", "tools/equipment", "4"):
+                    auto_sub_cat_name = "Tools/Equipment"
+                elif auto_sub_cat in ("electronics", "audio", "electronics/audio", "5"):
+                    auto_sub_cat_name = "Electronics/Audio"
+                elif auto_sub_cat in ("interior", "interior accessories", "6"):
+                    auto_sub_cat_name = "Interior Accessories"
+                elif auto_sub_cat in ("exterior", "exterior accessories", "7"):
+                    auto_sub_cat_name = "Exterior Accessories"
+                elif auto_sub_cat in ("performance", "performance parts", "8"):
+                    auto_sub_cat_name = "Performance Parts"
+                elif auto_sub_cat in ("maintenance", "maintenance parts", "9"):
+                    auto_sub_cat_name = "Maintenance Parts"
+                elif auto_sub_cat in ("accessories", "10"):
+                    auto_sub_cat_name = "Accessories"
+                elif auto_sub_cat in ("other", "11"):
+                    auto_sub_cat_name = "Other"
+                else:
+                    print("Sorry, that's not a valid option.")
+                    print("Please elect a valid option or enter its corresponding number.")
+                    continue
+
+                #confirm auto subcategory selection or restart
+                auto_sub_cat_name_answer = item_category_confirmation(auto_sub_cat_name)
+                if auto_sub_cat_name_answer == "y":
+                    auto_classification = auto_sub_cat_name
+                    break
+                elif auto_sub_cat_name_answer =="n":
+                    continue
+
+        #choose clothing subcategory
+        elif item_classification == "Clothing/Apparel":
+            while True:
+                print("What clothing/apparel subcategory does this item best fit?")
+                clothing_sub_cat = input("-Men's Clothing(1), -Women's Clothing(2) -Children's clothing(3) -Shoes(4) -Boots(5) -Jackets/Coats(6) -Dresses(7) -Suits/Formalwear(8) -Vintage Clothing(9) -Athletic Wear(10) -Workwear(11) -Accessories(12) -Designer/Luxury(13) -Other(14) ").strip().lower()
+                if clothing_sub_cat in ("men", "men's", "men's clothing", "1"):
+                    clothing_sub_cat_name = "Men's Clothing"
+                elif clothing_sub_cat in ("women", "women's", "women's clothing", "2"):
+                    clothing_sub_cat_name = "Women's Clothing"
+                elif clothing_sub_cat in ("children's", "children", "children's clothing", "3"):
+                    clothing_sub_cat_name = "Children's Clothing"
+                elif clothing_sub_cat in ("shoes", "shoe", "4"):
+                    clothing_sub_cat_name = "Shoes"
+                elif clothing_sub_cat in ("boots", "boot", "5"):
+                    clothing_sub_cat_name = "Boots"
+                elif clothing_sub_cat in ("jackets", "coats", "jackets/coats", "6"):
+                    clothing_sub_cat_name = "Jackets/Coats"
+                elif clothing_sub_cat in ("dresses", "dress", "7"):
+                    clothing_sub_cat_name = "Dresses"
+                elif clothing_sub_cat in ("suits", "formal", "formalwear", "suits/formalwear", "8"):
+                    clothing_sub_cat_name = "Suits/Formalwear"
+                elif clothing_sub_cat in ("vintage", "vintage clothing", "9"):
+                    clothing_sub_cat_name = "Vintage Clothing"
+                elif clothing_sub_cat in ("athletic", "athletic wear", "10"):
+                    clothing_sub_cat_name = "Athletic Wear"
+                elif clothing_sub_cat in ("work", "workwear", "11"):
+                    clothing_sub_cat_name = "Workwear"
+                elif clothing_sub_cat in ("accessories", "12"):
+                    clothing_sub_cat_name = "Accessories"
+                elif clothing_sub_cat in ("designer", "luxury", "designer/luxury", "13"):
+                    clothing_sub_cat_name = "Designer/Luxury"
+                elif clothing_sub_cat in ("other", "14"):
+                    clothing_sub_cat_name = "Other"
+                else:
+                    print("Sorry, that's not a valid option.")
+                    print("Please elect a valid option or enter its corresponding number.")
+                    continue
+
+                #confirm clothing subcategory selection or restart
+                clothing_sub_cat_name_answer = item_category_confirmation(clothing_sub_cat_name)
+                if clothing_sub_cat_name_answer == "y":
+                    clothing_classification = clothing_sub_cat_name
+                    break
+                elif clothing_sub_cat_name_answer == "n":
+                    continue
+
+        #choose home/furniture subcategory
+        elif item_classification == "Home/Furniture":
+            while True:
+                print("What home/furniture subcategory does this item best fit under?")
+                home_sub_cat = input("-Furniture(1) -Kitchen(2) -Appliances(3) -Home Decor(4) -Lighting(5) -Bedding(6) -Bathroom(7) -Storage/Organization(8) -Tools/Hardware(9) -Garden/Yard(10) -Office Furniture(11) -Home Improvement(12) -Other(13) ").strip().lower()
+
+        #choose collectible subcategory
+        elif item_classification == "Collectibles":
+            while True:
+                print("What collectibles subcategory does this item best fit under?")
+                collectible_sub_cat = input("-Trading Cards(1) -Coins/Currency(2) -Stamps(3) -Sports Memorabilia(4) -Autographs(5) -Action Figures(6) -Statues/Figurines(7) -Comics(8) -Vintage Collectibles(9) -Avertising/Memorobilia(10) -Antiques(11) -Rare/Valuable Items(12) -Other(13) ").strip().lower()
+
+        #choose toy/game subcategory
+        elif item_classification == "Toys/Games":
+            while True:
+                print("What toys/games subcategory does this item best fit under?")
+                toy_sub_cat = input("-Board Games(1) -Card Games(2) -Video Games(3) -Gaming Consoles(4) -Puzzles(5) -LEGO/Building Sets(6) -Action Figures(7) -Dolls(8) -RC Vehicles(9) -Outdoor Toys(10) -Educational Toys(11) -Plush(12) -Vintage Toys(13) -Other(14) ").strip().lower()
+
+        #choose books/media subcategory
+        elif item_classification == "Books/Media":
+            while True:
+                print("What books/media subcategory does this item best fit under?")
+                books_sub_cat = input("-Fiction(1) -Nonfiction(2) -Textbooks(3) -Academic/Professeional(4) -Children's Books(5) -Comics/Manga(6) -Rare/Collectible Books(7) -DVDs/Blu-Ray(8) -CDs(9) -Vinyl Records(10) -Video Games(11) -Magazines(12) -Other(13) ").strip().lower()
+
+        #choose sports/outdoors subcategory
+        elif item_classification == "Sports/Outdoors":
+            while True:
+                print("What sports/outdoors subcategory does this item best fit under?")
+                sports_sub_cat = input("-Camping(1) -Hiking(2) -Fishing(3) -Hunting(4) -Cycling(5) -Golf(6) -Fitness/Gym Equipmnet(7) -Running(8) -Water Sports(9) -Winter Sports(10) -Team Sports(11) -Sporting Goods(12) -Outdoor Recreation(13) -Other(14) ").strip().lower()
+
+        #choose jewelry/accessories subcategory
+        elif item_classification == "Jewelry/Accessories":
+            while True:
+                print("What jewelry/accessories subcategory does this item best fit under?")
+                jewelry_sub_cat = input("-Rings(1) -Necklaces(2) -Bracelets(3) -Earrings(4) -Watches(5) -Sunglasses(6) -Handbags/Purses(7) -Wallets(8) -Belts(9) -Hats(10) -Designer Accessories(11) -Fine Jewelry(12) -Costume Jewelry(13) -Other(14) ").strip().lower()
+
+        #choose beauty/personal care subcategory
+        elif item_classification =="Beauty/Personal Care":
+            while True:
+                print("What beauty/personal care subcategory does this item best fit under?")
+                beauty_sub_cat = input("-Skincare(1) -Haircare(2) -Makeup(3) -Fragrances(4) -Grooming(5) -Hair Tools(6) -Personal Care Appliances(7) -Bath/Body(8) -Beauty Accessories(9) -Professional/Spa Equipment(10) -Other(11) ").strip().lower()
+
+        #choose hobbies/crafts subcategory
+        elif item_classification == "Hobbies/Crafts":
+            while True:
+                print("What hobbies/crafts subcategory does this item best fit under?")
+                hobbies_sub_cat = input("-Sewing(1) -Knitting/Crochet(2) -Painting(3) -Drawing(4) -Scrapbooking(5) -Model Kits(6) -3D Printing(7) -Woodworking(8) -Musical Instruments(9) -Photography(10) -RC/Hobby Vehicles(11) -Craft Supplies(12) -Art Supplies(13) -Other(14) ").strip().lower()
 
 
 
 
 
     #multiple items choice loop
-    elif item_type_choice == "multiple":
+    '''elif item_type_choice == "multiple":
         while True:
             multiple_items_number = input("How many different items are you looking to buy? ").strip().lower()
             try:
                 multiple_items_number = int(multiple_items_number)
                 if multiple_items_number <= 0:
-                    print("sorry, ")
+                    print("sorry, ")'''
 
 
 
